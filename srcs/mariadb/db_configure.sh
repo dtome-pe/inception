04ec2f4912224c9b9829aebc36eee9ec3bd3df21
theonewhoknew <theonewhoknew@protonmail.com>
2024-02-29 11:11:38 +0100

    update

--- srcs/mariadb/db_configure.sh
+++ srcs/mariadb/db_configure.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
+mysqladmin -u root password $MYSQL_ROOT_PASSWORD
 
-mysqld
+mysql -u root -p
 #admin
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 mysql -e CREATE USER \`${MYSQL_ADMIN}\`@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 #user
 mysql -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`${MYSQL_ADMIN}\`@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
 mysql -e FLUSH PRIVILEGES
+
+mysqld --bind-address=0.0.0.0
 
